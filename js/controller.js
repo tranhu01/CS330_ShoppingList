e5b24f0a6e3defa7c2bb87f881451174cfe6b477
Hung Tran <tranhu01@luther.edu>
2018-03-06 11:01:16 -0600

    controller & model

---
+++ js/controller.js
@@ -0,0 +1,11 @@
+var shoppingModel = new ShoppingList()
+
+function clickedon() {
+    let rowcolids = ['itemname', 'qty', 'store', 'category', 'price', 'priority']
+    let vals = {}
+    for (let cid of rowcolids) {
+        vals[cid] = document.getElementById(cid).value;
+    }
+    let it = new Item(vals.itemname, vals.qty, vals.priority, vals.store, vals.category, vals.price)
+    shoppingModel.addItem(it)
+}
--- js/model.js
+++ js/model.js
@@ -1,5 +1,38 @@
-class Item {
+'use strict;'
+
+var ViewThing = require('./view')
+
+class Subject{
+    constructor(){
+        this.handlers = []
+    }
+
+    subsribe(fn){
+        this.handlers.push(fn);
+    }
+
+    unsubscribe(fn){
+        this.handlers = this.handlers.filter(
+            function(item){
+                if (item !== fn){
+                    return item;
+                }
+            }
+        );
+    }
+
+    publish(msg, someobj){
+        var scope = someobj || window;
+        for (let fn of this.handlers){
+            fn(scope, msg)
+        }
+    }
+}
+
+
+class Item extends Subject{
     constructor(name, quantity, priority, store, section, price) {
+        super()
         this.name = name;
         this.priority = priority;
         this.store = store;
@@ -7,18 +40,61 @@ class Item {
         this.price = price;
 
         this.purchased = false;
-
     }
+
+    get name(){
+        return this.name
+    }
+    set name(newName){
+        this.name = newName
+        this.publish("changeName", this)
+    }
+    get priority(){
+        return this.priority
+    }
+    set priority(newPriority){
+        this.priority = newPriority
+        this.publish("changePriority", this)
+    }
+    get store(){
+        return this.store
+    }
+    set store(newStore){
+        this.store = newStore
+        this.publish("changeStore", this)
+    }
+    get section(){
+        return this.section
+    }
+    set section(newSection){
+        this.section = newSection
+        this.publish("changeSection", this)
+    }
+    get price(){
+        return this.price
+    }
+    set price(newPrice){
+        this.price = newPrice
+        this.publish("changePrice", this)
+    }
+
 }
 
-
-class ShoppingList{
+class ShoppingList extends Subject{
     constructor() {
-        this.newItems = []
-        this.oldItems = [];
+        super()
+        this.Items = []
     }
 
+    get ShoppingList(){
+        return this.Items
+    }
+
+    get length(){
+        return this.Items.length
+    }
     addItem(it) {
-        this.newItems.push(it)
+        this.Items.push(it)
+        this.publish('Add Item', this)
     }
 }
